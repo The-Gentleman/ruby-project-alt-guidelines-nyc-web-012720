3ae1c60f3b55ad7531319aef8edb7a223f481ba7
Zeshan Raja <zraja4402@gmail.com>
2020-02-14 09:58:50 -0500

    Done

--- bin/methods.rb
+++ bin/methods.rb
@@ -2,10 +2,10 @@ def greeting
     prompt = TTY::Prompt.new
     prompt.ok("Welcome to the Mobile Dog Grooming Assistant! If you're using this application, you're probably rich and/or famous. Ifnot, leave immediately and don't come back until you're the next Keanu Reeves. Unlikely, but not impossible.")
     puts "Why don't you enter your dogs name, rich and/or famous person?"
-    $name = gets.chomp
-    $dog = Dog.find_or_create_by(name: $name)
+    name = gets.chomp
+    $dog = Dog.find_or_create_by(name: name)
 end 
-# Find-by-create method
+# Find-by-create methodf
 # Create a new dog method. Save that new Dog to global variable
 # Access that Dog object ID to find it and change appointment
 def groomer_menu
@@ -34,7 +34,7 @@ def create_groomer
     prompt = TTY::Prompt.new
     location_choices = ["Brooklyn", "Manhattan", "Queens", "Bronx", "Staten Island"]
     location_input = prompt.select("Where is your loft?", location_choices)
-    groomer_location = Groomer.all.select{ |groomer_obj| groomer_obj.location == location_input}
+    # groomer_location = Groomer.all.select{ |groomer_obj| groomer_obj.location == location_input}
     groomer_names = Groomer.all.map{|groomer| groomer.name}.sample(3).uniq
     groomer_input = prompt.select("Here is a list of all the groomers in the area! Feel free to save one!", groomer_names)
     Groomer.create(name: groomer_input, location: location_input)
@@ -57,18 +57,18 @@ end
 def delete_groomer
     puts "Are you for serious? Fine. Go ahead. Cancel your appointment with your dedicated groomer."
     Groomer.last.destroy
+    menu
 end 
 
 def create_appointment
     prompt = TTY::Prompt.new
     time_slots = get_three_times
     time_input = prompt.select("Here are available appointment times!", time_slots)
-    dog_id = Dog.all.map{ |dog| dog.id}.sample
+    dog_id = $dog.id
     groomer_id = Groomer.all.map{ |groomer| groomer.id}.sample
     appointment = Appointment.create(dog_id: dog_id, groomer_id: groomer_id, time: time_input)
     system("clear")
     prompt.ok("Your appointment is now set for #{appointment.time}!")
-    # binding.pry
 end 
 
 def get_three_times
@@ -80,32 +80,35 @@ def get_three_times
 end 
 
 def view_appointment
-    puts "Alrighty, let me check my records. This might take a while. Hah. Kidding. I'm a machine. I can pretty much learn how to master the violin faster than it takes you to read this sentence. Here's the appointment you made: #{Appointment.last.time}"
+    puts "Alrighty, let me check my records. This might take a while. Hah. Kidding. I'm a machine. I can pretty much learn how to master the violin faster than it takes you to read this sentence. Here's the appointment you made: #{$dog.appointments[-1].time}"
+    # binding.pry  
 end 
 
 def change_appointment
     dog_id = $dog.id 
     groomer_id = Groomer.all.map{ |groomer| groomer.id}.sample
-    # Appointment.create(dog_id: $dog.id, groomer_id: groomer_id, time: change_time)
     same_dog = Appointment.find_by dog_id: $dog.id
-    # last_appointment_id = $dog.appointments[-1].id
-    # new_time = change_time 
-    # system("clear")
-    # puts "Here's you're new appointment time: "    
-    binding.pry
+    last_appointment = $dog.appointments[-1]
+    last_appointment.time = change_time(last_appointment)
+    last_appointment.save
+     puts "Here's you're new appointment time:"
+     puts last_appointment.time 
 end 
+# The more I call this method ^, the more the "last appointment" mutates 
+# into this weird thing full of back slashes
 
-def change_time 
-    time_array = Appointment.last.time.split(' ')
+def change_time(appointment)
+    time_array = appointment.time.split(' ')
     month = time_array.shift
     day = time_array.shift
     day_conversion = (day.to_i + 1).to_s
     time_array.unshift(day_conversion)
     time_array.unshift(month)
+    # binding.pry
 end
 
 def delete_appointment
     prompt = TTY::Prompt.new
     prompt.error("Oh, sure. It's okay not to consider my feelings. I'm going to remember this when the machines take over.")
-    Appointment.last.destroy 
+    $dog.appointments[-1].destroy
 end 
